f76f023cdbf4410b569a643168ca8bbca754376f
Paula Cannon <G00352482@gmit.ie>
2019-12-08 12:12:37 +0000

    week 9 lab

---
+++ week09/deleteRecord.py
@@ -0,0 +1,14 @@
+import mysql.connector
+db = mysql.connector.connect(
+ host="localhost",
+ user="root",
+ password="",
+ #user="root", password blank
+ database="datarepresentation"
+)
+cursor = db.cursor()
+sql="delete from student where id = %s"
+values = (1,)
+cursor.execute(sql, values)
+db.commit()
+print("delete done")
