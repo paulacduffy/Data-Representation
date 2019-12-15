7139eec75aa7ea99430afe7e308ccc17f68dcf2e
Paula Cannon <G00352482@gmit.ie>
2019-12-15 15:10:12 +0000

    more troubleshooting

--- project/server.py
+++ project/server.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__, static_url_path='', static_folder='.')
 CORS(app)
 
-@app.route('/pageone')
+@app.route('/')
 def pageone():
     return render_template('pageone.html')
 
