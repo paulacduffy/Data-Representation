464fd75519cc9ba1bd5518e1559ce8df9655643c
Paula Cannon <G00352482@gmit.ie>
2019-12-20 12:47:34 +0000

    create register

--- project/server.py
+++ project/server.py
@@ -52,6 +52,22 @@ def stafflogin():
            
     return render_template('stafflogin.html', msg=msg)
 
+@app.route('stafflogin/register', methods=['GET', 'POST'])
+def register():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('register.html', msg=msg)
+
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
